1cddffe5bf96dfc29266f1c0020671ff4f345260
Zack <Zackcolemanmusic@gmail.com>
2017-10-23 09:42:10 +0100

    Album table added

--- db/vinyl_shop.sql
+++ db/vinyl_shop.sql
@@ -1,4 +1,16 @@
+DROP TABLE artists;
+DROP TABLE albums;
+
+
 CREATE TABLE artists(
   id SERIAL4 PRIMARY KEY,
   name VARCHAR(255)
+);
+
+CREATE TABLE albums(
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  quantity INT4,
+  artist_id INT4 REFERENCES artists(id) ON DELETE CASCADE,
 );
