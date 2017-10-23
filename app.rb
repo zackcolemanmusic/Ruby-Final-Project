c898bce3fee036231aebbbb653b92f00ec385283
Zack <Zackcolemanmusic@gmail.com>
2017-10-23 15:55:14 +0100

    Create artists complete

--- app.rb
+++ app.rb
@@ -19,3 +19,13 @@ get '/album' do # index
   @album = Album.all()
   erb( :album_index )
 end
+
+get '/artist/new' do
+  erb(:new_artist)
+end
+
+post '/artist/new' do
+  @artist = Artist.new(params)
+  @artist.save
+  erb(:create_artist)
+end
